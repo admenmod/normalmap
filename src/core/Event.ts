e1c5dbc812a9a033438f889d7962303b7f5de509
admenmod <admenmod@gmail.com>
2022-11-25 08:25:42 +0300

    fix v3.8

--- src/core/Event.ts
+++ src/core/Event.ts
@@ -63,10 +63,54 @@ export class EventEmitter {
 		((this as any)[`@${type}`] as Event<this, Args>).on(fn);
 	}
 
+	public once<
+		Type extends ConvertDel<KeysEvents<this>>,
+		Args extends ArgsEvent<this, Type>
+	>(type: Type, fn: (this: this, ...args: Args) => any) {
+		((this as any)[`@${type}`] as Event<this, Args>).once(fn);
+	}
+
+	public off<
+		Type extends ConvertDel<KeysEvents<this>>,
+		Args extends ArgsEvent<this, Type>
+	>(type: Type, fn: (this: this, ...args: Args) => any) {
+		((this as any)[`@${type}`] as Event<this, Args>).off(fn);
+	}
+
 	public emit<
 		Type extends ConvertDel<KeysEvents<this>>,
 		Args extends ArgsEvent<this, Type>
 	>(type: Type, ...args: Args) {
 		((this as any)[`@${type}`] as Event<this, Args>).emit(...args);
 	}
+
+	public clear<Type extends ConvertDel<KeysEvents<this>>>(type: Type) {
+		((this as any)[`@${type}`] as Event<this, any>).clear();
+	}
 }
+
+// пример использования
+class A extends EventEmitter {
+	public '@init' = new Event<A, ['init param']>(this);
+	public '@exit' = new Event<A, ['exit param']>(this);
+	public '@update' = new Event<A, [number]>(this);
+}
+
+let a = new A();
+
+a.emit('init', 'init param');
+a.emit('exit', 'exit param');
+
+a.on('update', function(dt) {
+	dt; // type: number
+	this; // type: A
+});
+
+
+a.off('exit', function(n) {
+	n;
+	this;
+});
+
+
+a.clear('init');
